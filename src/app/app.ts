13ff53257bba4aeba5f028b61e8d94a15733c0f0
Sergey Malyuga <sergey@malyuga.ru>
2025-09-22 21:18:34 +0300

    Добавить смысловые блоки для header.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainComponent} from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('strelka-express');
--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "extends": ["stylelint-config-standard-scss"]
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    "selector-class-pattern": [
+      "^[a-z]([a-z0-9-]+)?(__[a-z0-9-]+)?(--[a-z0-9-]+)?$",
+      {
+        "message": "Expected class selector to be kebab-case or BEM notation"
+      }
+    ]
+  }
 };
