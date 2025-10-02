8bfbcfa4ec7b17930e2e6666007994607ea0947f
Sergey Malyuga <sergey@malyuga.ru>
2025-10-02 21:27:10 +0300

    Создать компонент delivery-services.

---
+++ src/app/features/delivery-services/delivery-services.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-services',
+  templateUrl: './delivery-services.component.html',
+  styleUrl: './delivery-services.component.scss'
+})
+export class DeliveryServicesComponent {
+
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import {DeliveryServicesComponent} from '../../features/delivery-services/delivery-services.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  imports: [
+    DeliveryServicesComponent
+  ]
 })
 export class MainComponent {}
