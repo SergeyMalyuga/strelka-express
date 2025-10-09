d9c4fc9ed515ab134c358d67952f8ce477564970
Sergey Malyuga <sergey@malyuga.ru>
2025-10-09 20:16:02 +0300

    Создать tariffs.component. Сверстать section tariffs

---
+++ src/app/features/tariffs/tariffs.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tariffs',
+  imports: [],
+  templateUrl: './tariffs.component.html',
+  styleUrl: './tariffs.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TariffsComponent {
+
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DeliveryServicesComponent } from '../../features/delivery-services/delivery-services.component';
 import { NewsComponent } from '../../features/news/news.component';
 import { FulfillmentComponent } from '../../features/fulfillment/fulfillment.component';
+import {TariffsComponent} from '../../features/tariffs/tariffs.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  imports: [DeliveryServicesComponent, NewsComponent, FulfillmentComponent],
+  imports: [DeliveryServicesComponent, NewsComponent, FulfillmentComponent, TariffsComponent],
 })
 export class MainComponent {}
