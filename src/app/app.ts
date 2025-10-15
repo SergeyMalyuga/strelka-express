457d240607b13aa4af69be998c8a684b654a2d6a
Sergey Malyuga <sergey@malyuga.ru>
2025-10-15 14:37:01 +0300

    Убрать дублирующиеся стили.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainComponent } from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('strelka-express');
-}
+export class App {}
