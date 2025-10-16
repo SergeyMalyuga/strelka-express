e5067935c9fc59dae62befc30f022acf34720ca0
Sergey Malyuga <sergey@malyuga.ru>
2025-10-16 12:23:18 +0300

    Создать policy.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [{ path: '', component: MainComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Логистика для ваших грузов с услугами фулфилмента от StrelkaExpress.',
+    loadComponent: () => import('./pages/main/main.component').then((m) => m.MainComponent),
+  },
+  {
+    path: 'policy',
+    title:
+      'Политика конфиденциальности — Логистика для ваших грузов с услугами фулфилмента от StrelkaExpress',
+    loadComponent: () => import('./pages/policy/policy.component').then((m) => m.PolicyComponent),
+  },
+];
